a6399e73e53261aeb920f553a3b4adadfa9b8781
bbkbyti <bbkbyti@gmail.com>
2022-11-08 21:29:53 -0600

    login works

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
 
+import { useState } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import Layout from './layouts/Layout';
+import Login from './screens/Login';
+import { loginUser } from './services/auth';
+
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (loginData) => {
+    const userData = await loginUser(loginData);
+    setCurrentUser(userData);
+    navigate('/');
+  }
+
+
   return (
     <div className="App">
       <Layout>
-
+        <Routes>
+          <Route exact path='/login' element={<Login handleLogin={handleLogin} />} />
+        </Routes>
       </Layout>
     </div>
   );
--- client/src/layouts/Layout.js
+++ client/src/layouts/Layout.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Layout(props) {
     return (
         <header>
             <h1>Address Book</h1>
+            <Link to='/login'>Login/Register</Link>
+            <hr />
             {props.children}
         </header>
     )
